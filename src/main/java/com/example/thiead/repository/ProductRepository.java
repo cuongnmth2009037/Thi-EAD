ee40c9c9febe8c639cc04017d67fc5fe73b74a9a
ADMIN <cuongnmth2009037@fpt.edu.vn>
2022-07-30 23:16:36 +0700

    Upload File

---
+++ src/main/java/com/example/thiead/repository/ProductRepository.java
@@ -0,0 +1,8 @@
+package com.example.thiead.repository;
+
+import com.example.thiead.entity.Product;
+import com.example.thiead.entity.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProductRepository extends JpaRepository<Product, Long> {
+}
